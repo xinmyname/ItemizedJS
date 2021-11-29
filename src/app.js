158ea70460b5f7b27a9b1369ffe65ffd385ccecf
Andy Sherwood <xinmyname@gmail.com>
2021-11-28 16:25:24 -0800

    add service worker

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js')
+        .then((reg) => {
+            // registration worked
+            console.info('Registration succeeded. Scope is ' + reg.scope);
+        }).catch((error) => {
+            // registration failed
+            console.error('Registration failed with ' + error);
+        });
+}
+
 const main = document.querySelector("main");
 
 let ul = document.createElement("ul");
